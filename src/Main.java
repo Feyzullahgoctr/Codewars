84783e25ff7650b6da584dbcead55e79d6cd6614
fgoc <feyzullahgoctr@gmail.com>
2026-05-06 14:25:38 +0200

    Codewars1

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Helle World");
+    }
+
+    public static void printSomething() {
+        System.out.println("Print Something");
+    }
+
+}
